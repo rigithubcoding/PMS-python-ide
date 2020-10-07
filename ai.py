632c5bfa9b3c9d9dac7f6124c414904c9357e001
Ishan Roychowdhury <piklenews@gmail.com>
2020-10-07 10:50:07 -0400

    made it so the model dumps to external file

--- ai.py
+++ ai.py
@@ -5,9 +5,10 @@ from sklearn.feature_extraction.text import CountVectorizer
 keywords=["with", "import", "try", "except", "True", "False", "def", "class", "as", "from", "await", "pass",    "None", "break", "raise"]
 
 builtins=["abs", "ascii", "bin","open"]
+test_texts=["import", "abs"]
 
 training_texts = keywords + builtins
-training_labels = ["negative"] * len(negative_texts) + ["positive"] * len(positive_texts)
+training_labels = ["keyword"] * len(keywords) + ["builtin"] * len(builtins)
 
 vectorizer = CountVectorizer()
 vectorizer.fit(training_texts)
@@ -16,3 +17,14 @@ testing_vectors = vectorizer.transform(test_texts)
 classifier = tree.DecisionTreeClassifier()
 classifier.fit(training_vectors, training_labels)
 predictions = classifier.predict(testing_vectors)
+
+# Dump the trained decision tree classifier with Pickle
+decision_tree_pkl_filename = 'ai.pkl'
+# Open the file to save as pkl file
+decision_tree_model_pkl = open(decision_tree_pkl_filename, 'wb')
+pickle.dump(classifier, decision_tree_model_pkl)
+# Close the pickle instances
+decision_tree_model_pkl.close()
+# Dump the trained decision tree classifier with Pickle
+decision_tree_pkl_filename = 'decision_tree_classifier_20170212.pkl'
+decision_tree_model_pkl.close()
