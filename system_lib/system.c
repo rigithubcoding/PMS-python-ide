1cd8e44bf8e9b0f2c48f2d9f562f63b8cbcae33c
hi101000 <piklenews@gmail.com>
2020-10-09 07:39:49 -0400

    added stuff to system library

--- system_lib/system.c
+++ system_lib/system.c
@@ -1,3 +1,22 @@
-#define PY_SSIZE_T_CLEAN
+//#define 
+/*PY_SSIZE_T_CLEAN*/
 #include <python3.8/Python.h>
 
+static PyObject* helloworld(PyObject* self)
+{
+   return Py_BuildValue("s", "Hello, Python extensions!!");
+}
+
+static char helloworld_docs[] =
+   "helloworld( ): Any message you want to put here!!\n";
+
+static PyMethodDef helloworld_funcs[] = {
+   {"helloworld", (PyCFunction)helloworld, 
+   METH_NOARGS, helloworld_docs},
+   {NULL}
+};
+
+void inithelloworld(void)
+{
+   Py_InitModule3("helloworld", helloworld_funcs, "Extension module example!");
+}
